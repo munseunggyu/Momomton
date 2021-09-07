bb8f4719d9726a29abdca2f954c5943bc606a684
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2021-09-07 10:24:31 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@ const imgs = ['0.jpeg','1.jpeg']
 const chosenImage = imgs[Math.floor(Math.random()*imgs.length)]
 const bgImg = document.createElement('img')
 
-bgImg.src = `/images/${chosenImage}`
+bgImg.src = `images/${chosenImage}`
 
 document.body.appendChild(bgImg)
 
